2b53bbb333a1950429a2a015fb68938db385e24c
Harshada Bhondave <manishabhondave640@gmail.com>
2025-02-17 23:23:50 +0530

    made api for get transactions

--- Backend/resolvers/transaction.resolver.js
+++ Backend/resolvers/transaction.resolver.js
@@ -1,7 +1,31 @@
+import Transaction from "../models/transaction.model.js"
 
 const transationResolver ={
     Query:{
+        transactions : async(_,_,context) =>{
+           try {
+            if(!context.getUser()){
+                throw new Error("Unauthorized")
+            }
+            const userId = await context.getUser()._id
+            const transactions = await Transaction.find({userId})
+            return transactions
+           } catch (error) {
+             console.error("Error in getting Transactions",error);
+             throw new Error("Error getting Transactions");
+             
+           }
+        },
 
+        transaction : async(_,{transactionId},context) =>{
+            try {
+                const transaction = await Transaction.findById(transactionId)
+                return transaction
+            } catch (error) {
+                console.error("Error in getting Transaction",error);
+                 throw new Error("Error getting Transaction");
+            }
+        }
     },
     Mutation:{
 
