45f98a8d09f9408eac272788b519c523487d592f
Harshada Bhondave <manishabhondave640@gmail.com>
2025-03-14 00:03:22 +0530

    transaction resolver added

--- Backend/resolvers/transaction.resolver.js
+++ Backend/resolvers/transaction.resolver.js
@@ -2,7 +2,7 @@ import Transaction from "../models/transaction.model.js"
 
 const transationResolver ={
     Query:{
-        transactions : async(_,_,context) =>{
+        transactions : async(_, __,context) =>{
            try {
             if(!context.getUser()){
                 throw new Error("Unauthorized")
@@ -28,7 +28,44 @@ const transationResolver ={
         }
     },
     Mutation:{
+        createTransation: async (_,{input},context) =>{
+           try {
+            
+            const newTransaction = new Transaction({
+                ...input,
+                userId:context.getUser()._id
+            })
+            await newTransaction.save()
+            return newTransaction
 
+           } catch (error) {
+            console.error("Error creating Transaction", error.message);
+            throw new Error("Error creating Transaction")
+            
+           }
+
+        },
+        updateTransation: async (_,{input},) =>{
+          try {
+            const updateTransation = await Transaction.findByIdAndUpdate(input.transactionId, input , {new:true})
+            return updateTransation
+          } catch (error) {
+            console.error("Error updating Transaction", error);
+            throw new Error("Error updating Transaction")
+          }
+
+        },
+        
+        deleteTransation: async (_ ,{transactionId}) =>{
+          try {
+            const deleteTransation = await Transaction.findByIdAndDelete(transactionId)
+            return deleteTransation
+          } catch (error) {
+            console.error("Error deleting Transaction", error);
+            throw new Error("Error deleting Transaction")
+          }
+
+        }
     }
 }
 
